ac168e2d5f771b53aca1ac71e565af4db6c0acc7
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-06-15 23:07:05 +0300

    ft:link the url paths

--- awwwards/urls.py
+++ awwwards/urls.py
@@ -1,4 +1,16 @@
-from django.conf.urls import url,include
-from . import views
+from  django.urls import path
+from . import views 
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import routers
+
+
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    # path('logout/', views.logout, name='logout'),
+    path('new/profile/',views.new_profile, name='profile'),
+    path('project/', views.project, name='project'),
+    path('rating/<int:pk>/',views.rating,name='rating'),
+]
